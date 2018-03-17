d2b57f44de4408dfb8563274ebf7155a38a05b61
Johann Gail <johann.gail@mail.de>
2018-03-17 16:43:01 +0100

    Changed PLA preheat to 200 Degree

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1156,7 +1156,7 @@
 // @section temperature
 
 // Preheat Constants
-#define PREHEAT_1_TEMP_HOTEND 180
+#define PREHEAT_1_TEMP_HOTEND 200
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
